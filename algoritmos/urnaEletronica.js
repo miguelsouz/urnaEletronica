ab68e3baf08bfc3588eda9cb4cf619b7ad47a0ad
Miguelallan <miguel2020allan@gmail.com>
2023-10-11 14:57:35 +0000

    feat: estruturando codigo da urna 11/10

--- algoritmos/urnaEletronica.js
+++ algoritmos/urnaEletronica.js
@@ -1,19 +1,71 @@
 function urnaEletronica() {
     
-    let opcao, contadorTotalDeVotos = 0;
+    let opcao;
+    let contador = 0;
     
+    let nome1;
+    let nome2;
+    let nome3;
+
+    nome1 = parseInt(prompt('Digite o nome'));
+    nome2 = parseInt(prompt('Digite o nome'));
+    nome3 = parseInt(prompt('Digite o nome'));
+
+    let candidato1 = 0;
+    let candidato2 = 0;
+    let candidato3 = 0;
+    let votonulo = 0;
+    let votobranco = 0;
     do {
+        opcao = parseInt(prompt('Digite a opção'))
+            
+        console.log('Repetição');
+        contador++;
 
-        console.log('Repetição', contadorTotalDeVotos);
+        if (opcao ==1) {
+            
+            console.log('Opção do voto é o', nome1);
+            candidato1++;
 
-        opcao = parseInt(prompt('Digite a opção'));
+        }else if (opcao ==2) {
+            console.log('Opção do voto é o', nome2);
+            candidato2++;
 
-        contadorTotalDeVotos++;
+        }else if (opcao ==3) {
+            console.log('Opção do voto é o', nome3);
+            candidato3++; 
+
+        }else if (opcao ==5) {
+            console.log('Opção do voto é voto em branco');
+            votobranco++; 
+
+        }else if (opcao ==8) {
+            console.log('Opção do voto é nulo ');
+            votonulo++; 
+        }
+
 
     } while (opcao !== 0);
 
-    contadorTotalDeVotos--;
+    contador = contador - 1;
+    console.log('Contagem:', contador);
+    console.log('Este é o total de votos para o Candidato 1:', nome1,':', candidato1);
+    console.log('Este é o total de votos para o Candidato 2:', nome2,':', candidato2);
+    console.log('Este é o total de votos para o Candidato 3:', nome3,':', candidato3);
+    console.log('Este é o total de votos para o Voto em Branco:', votobranco);
+    console.log('Este é o total de votos para o Voto Nulo:', votonulo);
 
-    console.log('Contagem:', contadorTotalDeVotos);
+    if (candidato1 > candidato2 && candidato1 > candidato3) {
+        console.log('O ganhador é o', nome1);
+        console.log('total de Votos válidos e os votos Brancos', votobranco, candidato1);
+    }else if (candidato2 > candidato2 && candidato2 > candidato3) {
+        console.log('O ganhador é o', nome2);
+        console.log('Total de votos confirmados e os em Brancos', votobranco, candidato2);
+    }else if (candidato3 > candidato1 && candidato3 > candidato2) {
+        console.log('O ganhador é o', nome3);
+        console.log('Total de votos confirmados e os em Brancos', votobranco, candidato3);
+    }else {
+        console.log('Empate');
+    }
 
 }
